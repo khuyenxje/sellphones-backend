05ef7e2fcef55e43501e1afb8f2e568361210ade
khuyenxz <khuyenhoang1122333@gmail.com>
2025-12-14 05:12:16 +0000

    update file yml for startup faster v2

--- src/main/java/com/sellphones/configuration/ElasticsearchConfig.java
+++ src/main/java/com/sellphones/configuration/ElasticsearchConfig.java
@@ -4,22 +4,62 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.Setter;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 
 @Configuration
+@Setter
+@ConfigurationProperties(prefix = "spring.elastic")
+@ConditionalOnProperty(
+        prefix = "elastic",
+        name = "enabled",
+        havingValue = "true"
+)
 public class ElasticsearchConfig {
 
+    private String host;
+
+    private int port;
+
+    private String scheme;
+
+    private String username;
+
+    private String password;
+
     @Bean
-    public ElasticsearchClient client(){
-        RestClient restClient = RestClient.builder(HttpHost.create("http://localhost:9200")).build();
-        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+    public ElasticsearchClient client() {
+
+        BasicCredentialsProvider credentialsProvider =
+                new BasicCredentialsProvider();
+
+        credentialsProvider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password)
+        );
+
+        RestClient restClient = RestClient.builder(
+                        new HttpHost(host, port, scheme)
+                )
+                .setHttpClientConfigCallback(httpClientBuilder ->
+                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                )
+                .build();
+
+        ElasticsearchTransport transport =
+                new RestClientTransport(restClient, new JacksonJsonpMapper());
+
         return new ElasticsearchClient(transport);
     }
-
     @Bean
     public ElasticsearchTemplate elasticsearchTemplate(ElasticsearchClient client){
         return new ElasticsearchTemplate(client);
